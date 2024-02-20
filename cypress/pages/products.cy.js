3755ef62f4b5514db528287fec4a2034b32e3ba8
Kamil Barszczewski <kamiilbarszczewskii@gmail.com>
2024-02-20 16:04:28 +0100

    update products

--- cypress/pages/products.cy.js
+++ cypress/pages/products.cy.js
@@ -7,12 +7,27 @@ class Products {
     }
 
     get product1st() {
-        // cy.get('.single-products').contains('Blue Top').trigger('mouseover')
         return cy.get('a[data-product-id="1"]').contains('Add to cart');
     }
 
     get product2nd() {
         return cy.get('a[data-product-id="2"]').contains('Add to cart');
+    }
+
+    get jeans1() {
+        return cy.get('a[data-product-id="33"]').contains('Add to cart');
+    }
+
+    get jeans2() {
+        return cy.get('a[data-product-id="35"]').contains('Add to cart');
+    }
+
+    get jeans3() {
+        return cy.get('a[data-product-id="37"]').contains('Add to cart');
+    }
+
+    get btnAddToCart() {
+        return cy.get('button > .fa-shopping-cart').contains('Add to cart');
     }
 
     get1Product() {
@@ -25,6 +40,31 @@ class Products {
 
     getSecondProduct() {
         this.product2nd.click();
+    }
+
+    getJeans1() {
+        cy.contains('p', 'Soft Stretch Jeans').should('be.visible');
+        this.jeans1.click();
+    }
+
+    getJeans2() {
+        cy.contains('p', 'Regular Fit Straight Jeans').should('be.visible');
+        this.jeans2.click();
+    }
+
+    getJeans3() {
+        cy.contains('p', 'Grunt Blue Slim Fit Jeans').should('be.visible');
+        this.jeans3.click();
+    }
+
+    verifyAllJeans() {
+        cy.contains('a', 'Soft Stretch Jeans').should('be.visible');
+        cy.contains('a', 'Regular Fit Straight Jeans').should('be.visible');
+        cy.contains('a', 'Grunt Blue Slim Fit Jeans').should('be.visible');
+    }
+
+    addToCart() {
+        this.btnAddToCart.click();
     }
 
     displayFirstProduct() {
