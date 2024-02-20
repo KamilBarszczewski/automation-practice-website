241229d07cbac93ae15b9eecbea7341483355d58
Kamil Barszczewski <kamiilbarszczewskii@gmail.com>
2024-02-20 16:03:12 +0100

    update page

--- cypress/component/pageComponent.js
+++ cypress/component/pageComponent.js
@@ -1,21 +1,34 @@
 /// <reference types="cypress" />
 
+import Cart from "../pages/cart.cy";
+import Checkout from "../pages/checkout.cy";
+import ContactUs from "../pages/contactUs.cy";
+import Payment from "../pages/payment.cy";
+import Products from "../pages/products.cy";
+
 class PageComponent {
 
+
+    products = new Products();
+    payment = new Payment();
+    checkout = new Checkout();
+    cart = new Cart();
+    contactUs = new ContactUs();
+
     get inputSearch() {
-        return cy.get('#search_product')
+        return cy.get('#search_product');
     }
 
     get inputSubscribe() {
-        return cy.get('#susbscribe_email')
+        return cy.get('#susbscribe_email');
     }
 
     get btnSearch() {
-        return cy.get('#submit_search')
+        return cy.get('#submit_search');
     }
 
     get btnSubscribe() {
-        return cy.get('#subscribe')
+        return cy.get('#subscribe');
     }
 
     get btnContinueShopping() {
@@ -27,14 +40,37 @@ class PageComponent {
         return cy.get('a[href="/view_cart"] > u');
     }
 
+    get linkWomen() {
+        return cy.get('a[href="#Women"]');
+    }
+
+    get linkMen() {
+        return cy.get('a[href="#Men"]');
+    }
+
+    get linkDress() {
+        return cy.get('a[href="/category_products/1"]').contains('Dress');
+    }
+
+    get linkTshirts() {
+        return cy.get('a[href="/category_products/3"]').contains('Tshirts');
+    }
+
+    get linkPolo() {
+        return cy.get('a[href="/brand_products/Polo"]').contains('Polo');
+    }
+
+    get linkHM() {
+        return cy.get('a[href="/brand_products/H&M"]').contains('H&M');
+    }
+
     continueShopping() {
         cy.contains('h4', 'Added!').should('be.visible');
-        this.btnContinueShopping.click()
+        this.btnContinueShopping.click();
     }
     
     searchProduct(productName) {
         cy.contains('h2', 'All Products').should('be.visible');
-        cy.get('.features_items').should('be.visible');
         this.inputSearch.clear().type(productName);
         this.btnSearch.click();
         cy.contains('h2', 'Searched Products').should('be.visible');
@@ -48,10 +84,37 @@ class PageComponent {
     }
 
     viewCart() {
-        cy.contains('u', 'View Cart').should('be.visible')
-        this.linkViewCart.click()
+        cy.contains('u', 'View Cart').should('be.visible');
+        this.linkViewCart.click();
     }
 
+    categoryWomen() {
+        this.linkWomen.click();
+    }
+
+    categoryMen() {
+        this.linkMen.click();
+    }
+
+    subcategoryDress() {
+        this.linkDress.click();
+        cy.contains('h2', 'Women - Dress Products');
+    }
+
+    subcategoryTshirts() {
+        this.linkTshirts.click();
+        cy.contains('h2', 'Men - Tshirts Products');
+    }
+
+    brandPolo() {
+        this.linkPolo.click();
+        cy.contains('h2', 'Brand - Polo Products');
+    }
+
+    brandHM() {
+        this.linkHM.click();
+        cy.contains('h2', 'Brand - H&M Products');
+    }
 }
 
 export default PageComponent;
