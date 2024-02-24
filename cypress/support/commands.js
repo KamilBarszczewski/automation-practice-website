029d6122832b39f25b077422da7ef4f584adc964
Kamil Barszczewski <kamiilbarszczewskii@gmail.com>
2024-02-24 22:43:18 +0100

    add getProduct and getSubpage

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,11 @@
-Cypress.Commands.add('getDataQa', (dataQaSelector) => {
-    return cy.get(`[data-qa="${dataQaSelector}"]`)
-})
+Cypress.Commands.add("getDataQa", (dataQaSelector) => {
+  return cy.get(`[data-qa="${dataQaSelector}"]`);
+});
+
+Cypress.Commands.add("getProduct", (productNumber) => {
+  return cy.get(`[href="/product_details/${productNumber}"]`).click();
+});
+
+Cypress.Commands.add("getSubpage", (sub_page) => {
+  return cy.get(`[href="/${sub_page}"]`);
+});
