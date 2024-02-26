d57d4cd8f2389380bf433bd1494179b4bdfd316e
Kamil Barszczewski <kamiilbarszczewskii@gmail.com>
2024-02-26 16:51:38 +0100

    add logic to verify product details

--- cypress/pages/cart.cy.js
+++ cypress/pages/cart.cy.js
@@ -8,6 +8,42 @@ class Cart {
   get btnSubscribe() {
     return cy.get("#subscribe");
   }
+git add .
+  get productPrice() {
+    return cy.get('td[class="cart_price"] > p');
+  }
+
+  get productQuantity() {
+    return cy.get('button[class="disabled"]');
+  }
+
+  get totalPrice() {
+    return cy.get('p[class="cart_total_price"]');
+  }
+
+  getProductPrice(productId) {
+    cy.get(`#product-${productId}`).within(() => {
+      this.productPrice.then((price) => {
+        cy.contains(price.text());
+      });
+    });
+  }
+
+  getProductQuantity(productId) {
+    cy.get(`#product-${productId}`).within(() => {
+      this.productQuantity.then((quantity) => {
+        cy.contains(quantity.text());
+      });
+    });
+  }
+
+  getTotalPrice(productId) {
+    cy.get(`#product-${productId}`).within(() => {
+      this.totalPrice.then((total) => {
+        cy.contains(total.text());
+      });
+    });
+  }
 
   get btnRemoveFirst() {
     return cy.get('a[data-product-id="1"]');
@@ -30,6 +66,10 @@ class Cart {
     this.inputSubscribe.scrollIntoView().clear().type(email);
     this.btnSubscribe.click();
     cy.get("#success-subscribe").should("be.visible");
+  }
+
+  getPrice(id) {
+    cy.get(`#product-${id}`).contains(this.productPrice);
   }
 
   removeFirstProduct() {
