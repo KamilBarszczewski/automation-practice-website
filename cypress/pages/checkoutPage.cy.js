1423d21655da35f52099f953bcb2bf82978156bc
Kamil Barszczewski <kamiilbarszczewskii@gmail.com>
2024-02-29 13:08:56 +0100

    update file

--- cypress/pages/checkoutPage.cy.js
+++ cypress/pages/checkoutPage.cy.js
@@ -15,6 +15,23 @@ class CheckoutPage {
     this.inputMessage.clear().type(message);
     this.btnPlaceOrder.click();
   }
+
+  verifyDeliveryAddress() {
+    cy.get("#address_delivery").within(() => {
+      cy.contains(/Mr. Testeusz Testeuszewicz/i).should("exist");
+      cy.contains(/Test-Soft Testowa 11 Testowa 21/i).should("exist");
+      cy.contains(/Testowo Testowe Nevada 555333111/i).should("exist");
+      cy.contains(/United States 0021455535299/i).should("exist");
+    });
+  }
+
+  verifyInvoiceAddress() {
+    cy.get("#address_invoice").within(() => {
+      cy.contains(
+        /Mr. Testeusz Testeuszewicz Test-Soft Testowa 11 Testowa 21 Testowo Testowe Nevada 555333111 United States 0021455535299/i
+      ).should("exist");
+    });
+  }
 }
 
 export default CheckoutPage;
