69409f2342a67d7ee772a1554572e8295f123010
Kamil Barszczewski <kamiilbarszczewskii@gmail.com>
2024-03-12 17:04:48 +0100

    update

--- cypress/e2e/api-post.cy.js
+++ cypress/e2e/api-post.cy.js
@@ -17,33 +17,33 @@ describe("automationexercise", () => {
   });
 });
 
-// describe("POST automationexercise", () => {
-//   it("POST to search product", () => {
-//     cy.request({
-//       url: "https://automationexercise.com/api/searchProduct",
-//       method: "POST",
-//       qs: { search_product: "top" },
-//     }).as("searchProduct");
+describe("POST automationexercise", () => {
+  it("POST to search product", () => {
+    cy.request({
+      url: "https://automationexercise.com/api/searchProduct",
+      method: "POST",
+      qs: { search_product: "top" },
+    }).as("searchProduct");
 
-//     cy.get("@searchProduct").its("status").should("equal", 200);
-//     cy.get("@searchProduct").then((response) => {
-//       cy.log(JSON.stringify(response.body));
-//     });
-//   });
+    cy.get("@searchProduct").its("status").should("equal", 200);
+    cy.get("@searchProduct").then((response) => {
+      cy.log(JSON.stringify(response.body));
+    });
+  });
 
-//   it.skip("verify login with valid details", () => {
-//     cy.request({
-//       url: "/api/verifyLogin",
-//       method: "POST",
-//       body: {
-//         username: "tescik@test.ts",
-//         password: "testowy@",
-//       },
-//     }).as("loginUser");
+  it.skip("verify login with valid details", () => {
+    cy.request({
+      url: "/api/verifyLogin",
+      method: "POST",
+      body: {
+        username: "tescik@test.ts",
+        password: "testowy@",
+      },
+    }).as("loginUser");
 
-//     cy.get("@loginUser").its("status").should("equal", 200);
-//     cy.get("@loginUser").then((response) => {
-//       cy.log(JSON.stringify(response.body));
-//     });
-//   });
-// });
+    cy.get("@loginUser").its("status").should("equal", 200);
+    cy.get("@loginUser").then((response) => {
+      cy.log(JSON.stringify(response.body));
+    });
+  });
+});
