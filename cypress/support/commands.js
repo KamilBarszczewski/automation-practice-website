5c35c67cf93d8541964a52efd3c2c1643c3795a9
Kamil Barszczewski <kamiilbarszczewskii@gmail.com>
2024-03-12 16:50:25 +0100

    list of commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,15 +2,15 @@ Cypress.Commands.add("getDataQa", (dataQaSelector) => {
   return cy.get(`[data-qa="${dataQaSelector}"]`);
 });
 
-Cypress.Commands.add("getProduct", (productNumber) => {
+Cypress.Commands.add("goToProductDetails", (productNumber) => {
   return cy.get(`[href="/product_details/${productNumber}"]`).click();
 });
 
-Cypress.Commands.add("getSubpage", (sub_page) => {
-  return cy.get(`[href="/${sub_page}"]`);
+Cypress.Commands.add("linkToPage", (endpoint) => {
+  return cy.get(`[href="/${endpoint}"]`);
 });
 
-Cypress.Commands.add("addToCart", (productNumber) => {
+Cypress.Commands.add("addProductToCartById", (productNumber) => {
   return cy.get('div[class="features_items"]').within(() => {
     cy.get(`a[data-product-id="${productNumber}"]`)
       .contains("Add to cart")
@@ -18,16 +18,3 @@ Cypress.Commands.add("addToCart", (productNumber) => {
       .click();
   });
 });
-
-// Cypress.Commands.add("addToCart", (productNumber) => {
-//   return cy.get('div[class="features_items"]').within(() => {
-//     cy.get('div[class="single-products"]')
-//       .its(productNumber - 1)
-//       .within(() => {
-//         cy.get(`a[data-product-id="${productNumber}"]`)
-//           .contains("Add to cart")
-//           .invoke("show")
-//           .click();
-//       });
-//   });
-// });
